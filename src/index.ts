1af3472f60d7d662d975e007e4bf1d1bdad23afa
iamVihanga <vihangarashansilva@gmail.com>
2025-04-01 11:13:40 +0530

    Add: Stoker - Error Handler & Not Found Handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
 
 const app = new OpenAPIHono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+// Error Handelling Middleware
+app.onError(onError);
+
+// Not Found Handelling Middleware
+app.notFound(notFound);
 
 export default app;
