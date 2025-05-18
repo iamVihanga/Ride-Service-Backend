eb85ac8f8a361e31c1addba584f1955c2b91c3e9
iamVihanga <vihangarashansilva@gmail.com>
2025-05-18 14:20:37 +0530

    Add: OTP Save on Database Setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { admin, bearer, openAPI, phoneNumber } from 'better-auth/plugins';
 
 import { db } from '@/db';
+import { otpList } from '@/db/schema';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -14,9 +15,23 @@ export const auth = betterAuth({
   // Better Auth Plugins
   plugins: [
     phoneNumber({
-      sendOTP: ({ phoneNumber, code }) => {
+      sendOTP: async ({ phoneNumber, code }) => {
         // TODO: Implement sending OTP code via SMS
         console.log(`Sending OTP code ${code} to phone number ${phoneNumber}`);
+
+        await db.insert(otpList).values({
+          code,
+          phoneNumber,
+        });
+      },
+      signUpOnVerification: {
+        getTempEmail: (phoneNumber) => {
+          return `${phoneNumber}@donext.org`;
+        },
+        //optionally, you can also pass `getTempName` function to generate a temporary name for the user
+        getTempName: (phoneNumber) => {
+          return phoneNumber; //by default, it will use the phone number as the name
+        },
       },
     }),
     admin(),
