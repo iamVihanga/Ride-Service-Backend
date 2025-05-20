c93b5440f297c833c2d2f6e9a9599f849c460af2
iamVihanga <vihangarashansilva@gmail.com>
2025-05-20 16:05:27 +0530

    Add: Payments, Payment Method API Completion

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import createApp from '@/lib/create-app';
 import configureOpenAPI from '@/lib/open-api-config';
 import drivers from '@/routes/drivers/drivers.index';
 import index from '@/routes/index.route';
+import payments, { methodsRouter } from '@/routes/payments/payments.index';
 import vehicles from '@/routes/vehicles/vehicles.index';
 
 const app = createApp();
@@ -14,7 +15,9 @@ const _app = app
   .route('/', index)
   // .route('/tasks', tasks)
   .route('/drivers', drivers)
-  .route('/vehicles', vehicles);
+  .route('/vehicles', vehicles)
+  .route('/payments', payments)
+  .route('/payment-methods', methodsRouter);
 
 export type AppType = typeof _app;
 
